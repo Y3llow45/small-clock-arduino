ffbfb1f806f351a5138d724fa0ac5372160db5e9
Yellow <bofengejn@gmail.com>
2023-05-28 13:09:25 +0300

    Update no-lib.ino

--- no-lib/no-lib.ino
+++ no-lib/no-lib.ino
@@ -1,8 +1,7 @@
-const int SHIFT_PIN_SHCP = 6;  // Shift register clock pin (SHCP)
-const int SHIFT_PIN_STCP = 5;  // Shift register latch pin (STCP)
-const int SHIFT_PIN_DS = 4;    // Shift register data pin (DS)
+const int latchPin = 5;   // Shift register latch pin
+const int clockPin = 6;   // Shift register clock pin
+const int dataPin = 4;    // Shift register data pin
 
-// Define the segment patterns for each digit (common cathode)
 const byte digitPatterns[] = {
   B00111111,  // Digit 0
   B00000110,  // Digit 1
@@ -16,54 +15,45 @@ const byte digitPatterns[] = {
   B01101111   // Digit 9
 };
 
-void shiftOutPattern(byte pattern) {
-  // Shift out a byte pattern to the shift register
-  digitalWrite(SHIFT_PIN_SHCP, LOW);
-  shiftOut(SHIFT_PIN_DS, SHIFT_PIN_SHCP, LSBFIRST, pattern);
-  digitalWrite(SHIFT_PIN_SHCP, HIGH);
-}
-
-void displayDigit(int digit, int value) {
-  // Display a single digit on the 7-segment display
-  shiftOutPattern(digitPatterns[value]);
-  digitalWrite(SHIFT_PIN_STCP, LOW);
-  digitalWrite(digit, LOW);
-  digitalWrite(SHIFT_PIN_STCP, HIGH);
-  digitalWrite(digit, HIGH);
-}
+const int digitPins[] = {
+  12,  // Digit 1
+  11,  // Digit 2
+  10,  // Digit 3
+  9    // Digit 4
+};
 
 void setup() {
-  pinMode(SHIFT_PIN_SHCP, OUTPUT);
-  pinMode(SHIFT_PIN_STCP, OUTPUT);
-  pinMode(SHIFT_PIN_DS, OUTPUT);
+  Serial.begin(9600);
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
 
-  // Set digit pins as outputs
-  pinMode(12, OUTPUT);  // Digit 0
-  pinMode(11, OUTPUT);  // Digit 1
-  pinMode(10, OUTPUT);  // Digit 2
-  pinMode(9, OUTPUT);   // Digit 3
+  for (int i = 0; i < 4; i++) {
+    pinMode(digitPins[i], OUTPUT);
+  }
 }
 
 void loop() {
-  // Example: Display the number 1234 on the 4-digit 7-segment display
-  int number = 1234;
+  displayDigits(5812);
+}
 
-  // Convert the number to individual digits
-  int digit1 = 1;
-  int digit2 = 2;
-  int digit3 = 3;
-  int digit4 = 4;
-
-  // Display each digit sequentially
-  displayDigit(12, digit1);  // Digit 0
-  delay(5);  // Adjust the delay as needed for brightness
-  displayDigit(11, digit2);  // Digit 1
-  delay(5);  // Adjust the delay as needed for brightness
-  displayDigit(10, digit3);  // Digit 2
-  delay(5);  // Adjust the delay as needed for brightness
-  displayDigit(9, digit4);   // Digit 3
-  delay(5);  // Adjust the delay as needed for brightness
-
-  // Delay before refreshing the display
-  //delay(1000);  // Delay for one second (adjust if needed)
+void displayDigits(int number) {
+  int digits[4];
+  
+  digits[0] = number / 1000;
+  digits[1] = (number / 100) % 10;
+  digits[2] = (number / 10) % 10;
+  digits[3] = number % 10;
+  
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, digitPatterns[digits[i]]);
+    digitalWrite(latchPin, HIGH);
+    
+    for (int j = 0; j < 4; j++) {
+      digitalWrite(digitPins[j], i == j ? LOW : HIGH);
+    }
+    
+    delay(5);
+  }
 }
